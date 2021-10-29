60ac31e06a71e5d36a20e74977ee2be0655d8672
emszi <emszi@LAPTOP-KMGBVH52>
2021-10-29 22:09:44 +0200

    Create MNBSoap, 4ik feladatig

---
+++ MNDSoap/MNDSoap/Form1.cs
@@ -0,0 +1,38 @@
+﻿using MNDSoap.Entities;
+using MNDSoap.MNBServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MNDSoap
+{
+    public partial class Form1 : Form
+    {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        public Form1()
+        {
+            InitializeComponent();
+            Consume();
+            dataGridView1.DataSource = Rates;
+        }
+        void Consume()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+            var response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            File.WriteAllText("export.xml", result);
+        }
+        
+    }
+}
