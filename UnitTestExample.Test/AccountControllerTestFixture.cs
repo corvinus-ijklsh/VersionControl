4f89557c446f20d55fd3433bfcd12045cfcc93e9
emszi <emszi@LAPTOP-KMGBVH52>
2021-12-10 23:13:55 +0100

    Testek

---
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [
+            Test,
+            TestCase("abcd","false"),
+            TestCase("abcdxyz.com","true"),
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            //Arrange
+            var accountController = new AccountController();
+            //Act
+            var result = accountController.ValidateEmail(email);
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
