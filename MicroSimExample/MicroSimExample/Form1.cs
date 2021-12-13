2729459e1bc1565ee3e6826c690e9bc5e3a158d2
emszi <emszi@LAPTOP-KMGBVH52>
2021-12-13 14:13:10 +0100

    Lista feltöltése, tesztfájl beolvasása

--- MicroSimExample/MicroSimExample/Form1.cs
+++ MicroSimExample/MicroSimExample/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MicroSimExample.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,38 @@ namespace MicroSimExample
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = null;
+        List<BirthProbability> BirthProbabilities = null;
+        List<DeathProbability> DeathProbabilities = null;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            Population = GetPopulation(@"C:\temp\nép-teszt.csv");
+        }
+
+        public List<Person> GetPopulation(string csvPath)
+        {
+            List<Person> population = new List<Person>();
+            using (var sr = new StreamReader(csvPath,Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                      
+                    
+                }
+            }
+
+            return population;
         }
     }
 }
